47d45aff0aeb07b17211a17339efd5e5559eb6a7
Steven Torrence <storrence88@gmail.com>
2018-09-30 20:50:01 -0500

    Updated Prettier settings in editor and added button element to Counter class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-  render() { 
-    return <h1>Hello World</h1>;
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
   }
 }
- 
+
 export default Counter;
